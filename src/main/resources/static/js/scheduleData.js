776635ba1c2452c87b815659a0d3689656efe3ab
Eshreat <j-eshreat@athuman.com>
2019-10-25 16:17:48 +0900

    add schedule file

---
+++ src/main/resources/static/js/scheduleData.js
@@ -0,0 +1,17 @@
+var data = [
+    {
+        text: "HA Class",
+        startDate: new Date(2019, 9, 23, 09, 00),
+        endDate: new Date(2019, 9, 23, 12, 00)
+    }, {
+        text: "HA Class",
+        startDate: new Date(2019, 9, 25, 09, 00),
+        endDate: new Date(2019, 9, 25, 12, 00)
+    },
+    {
+        text: "HA Class",
+        startDate: new Date(2019, 9, 29, 09, 00),
+        endDate: new Date(2019, 9, 29, 12, 00)
+    }
+];
+  
